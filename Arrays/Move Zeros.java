c57104a6455821a8ebc0459e54e7381d04c93c0b
tonyharsha <tonyharsha87@gmail.com>
2023-12-25 00:33:10 +0530

    Removing Comments

--- Arrays/Move Zeros.java
+++ Arrays/Move Zeros.java
@@ -3,7 +3,7 @@ class Solution {
         //Brute Force
         //TC O(N^2)
         //SC O(1).
-        /*
+        
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 for(int j=i+1;j<nums.length;j++){
@@ -16,7 +16,7 @@ class Solution {
                 }
             }
         }
-        */
+        
 
         //Optimal
         //TC O(N)
